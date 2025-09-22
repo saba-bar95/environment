f180c6b3696db91ba97d10bca738fa25af95fd7a
saba-bar95 <s.barbakadze95@gmail.com>
2025-09-22 16:07:00 +0400

    add scrolling to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./assets/components/Header/Header";
 import Footer from "./assets/components/Footer/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main className="main">
         <Outlet />
