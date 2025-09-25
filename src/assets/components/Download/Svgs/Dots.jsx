74f4eaca4ec59ad7dae3acd24d7c5e71b438283c
saba-bar95 <s.barbakadze95@gmail.com>
2025-09-25 15:28:07 +0400

    seperate svgs from download component

---
+++ src/assets/components/Download/Svgs/Dots.jsx
@@ -0,0 +1,38 @@
+const Dots = () => {
+  return (
+    <svg
+      width="36"
+      height="32"
+      viewBox="0 0 36 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <rect x="0.5" y="0.5" width="35" height="31" rx="11.5" stroke="#E0E0E0" />
+      <ellipse
+        cx="10.9565"
+        cy="16"
+        rx="2"
+        ry="1.95652"
+        transform="rotate(-90 10.9565 16)"
+        fill="#878787"
+      />
+      <ellipse
+        cx="17.9995"
+        cy="16"
+        rx="2"
+        ry="1.95652"
+        transform="rotate(-90 17.9995 16)"
+        fill="#878787"
+      />
+      <ellipse
+        cx="25.0434"
+        cy="16"
+        rx="2"
+        ry="1.95652"
+        transform="rotate(-90 25.0434 16)"
+        fill="#878787"
+      />
+    </svg>
+  );
+};
+
+export default Dots;
