f8ac3ef5e64e08ef4cc0e1669f400c9035ddd8bf
saba-bar95 <s.barbakadze95@gmail.com>
2025-10-15 16:06:40 +0400

    fix import bug

--- src/assets/components/Pages/Climate/Emissions/Emissions.jsx
+++ src/assets/components/Pages/Climate/Emissions/Emissions.jsx
@@ -2,7 +2,6 @@
 import { useParams } from "react-router-dom";
 import backgroundImg from "./Background/background.jpg";
 import GasEmissionsCard from "./GasEmissionsPage.jsx";
-import "./Emissions.scss";
 import RangeChartPage from "./RangeChartPage.jsx";
 import SectorDonutPage from "./SectorDonutPage.jsx";
 import IntensityChartPage from "./IntensityChartPage.jsx";
@@ -15,46 +14,46 @@ import IndexChartPage from "./IndexChartPage.jsx";
 import PercentageChartPage from "./PercentageChartPage.jsx";
 
 const Emissions = () => {
-    const { language } = useParams();
+  const { language } = useParams();
 
-    return (
-        <div className="section-container emissions">
-            <div
-                className="background-container"
-                style={{
-                    backgroundImage: `url(${backgroundImg})`,
-                    backgroundPosition: "center 40%",
-                }}>
-                <div className="overlay"></div>
-                <h1>
-                    {language === "en"
-                        ? "Greenhouse Gas Emissions in Georgia"
-                        : "სათბურის აირების გაფრქვევები საქართველოში"}
-                </h1>
-                <h2>
-                    {language === "en"
-                        ? "Interactive dashboard of emissions for the period from 2000 to 2017"
-                        : "გაფრქვევების ინტერაქტიული პანელი 2000-დან 2017 წლამდე პერიოდისთვის"}
-                </h2>
-            </div>
+  return (
+    <div className="section-container emissions">
+      <div
+        className="background-container"
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundPosition: "center 40%",
+        }}>
+        <div className="overlay"></div>
+        <h1>
+          {language === "en"
+            ? "Greenhouse Gas Emissions in Georgia"
+            : "სათბურის აირების გაფრქვევები საქართველოში"}
+        </h1>
+        <h2>
+          {language === "en"
+            ? "Interactive dashboard of emissions for the period from 2000 to 2017"
+            : "გაფრქვევების ინტერაქტიული პანელი 2000-დან 2017 წლამდე პერიოდისთვის"}
+        </h2>
+      </div>
 
-            <div className="charts-section">
-                <div className="charts-grid">
-                    <GasEmissionsCard />
-                    <RangeChartPage/>
-                    <SectorDonutPage/>
-                    <IntensityChartPage/>
-                    <StackedChartPage/>
-                    <EnergyChartPage/>
-                    <LulucfChartPage/>
-                    <GdpScatterPage/>
-                    <HfcScatterPage/>
-                    <IndexChartPage/>
-                    <PercentageChartPage/>
-                </div>
-            </div>
+      <div className="charts-section">
+        <div className="charts-grid">
+          <GasEmissionsCard />
+          <RangeChartPage />
+          <SectorDonutPage />
+          <IntensityChartPage />
+          <StackedChartPage />
+          <EnergyChartPage />
+          <LulucfChartPage />
+          <GdpScatterPage />
+          <HfcScatterPage />
+          <IndexChartPage />
+          <PercentageChartPage />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Emissions;
